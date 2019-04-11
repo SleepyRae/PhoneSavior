bb225efac1ebb5f9e9e02dff7e2ba3c474d6bc66
SleepyRae <794788034@qq.com>
2019-04-11 17:16:36 +0800

    facedetect v1.0 : 准确性存在问题

--- app/src/main/java/com/example/inspiron/phonesavior/ui/CameraActivity.java
+++ app/src/main/java/com/example/inspiron/phonesavior/ui/CameraActivity.java
@@ -7,11 +7,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.media.FaceDetector;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,6 +22,7 @@ import android.view.WindowManager;
 import android.widget.Toast;
 import com.example.inspiron.phonesavior.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -32,10 +35,9 @@ public class CameraActivity extends Activity {
     private SurfaceView mySurfaceView;
     private SurfaceHolder myHolder;
     private Camera myCamera;
-    private List<Integer> mWaitAction = new LinkedList<>(); //暂存拍照的队列
-    private boolean isTaking = false;   //是否处于拍照中
     private int num = 0;
     public static WeakReference<CameraActivity> weak = null;
+    private boolean isTaking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,18 +64,29 @@ public class CameraActivity extends Activity {
             public void run() {
                 //初始化camera并对焦拍照
                 //initCamera();
-                boolean result = false;
+             //   boolean result = false;
 
-                while(!result) {
+                while(!isTaking) {
+                    num++;
+                    Log.d("Demo", "第"+ num +"次开始拍照" );
+                    initCamera();
+
                     try {
-                        num++;
-                        Log.d("Demo", "第"+ num +"次开始拍照" );
-                        initCamera();
-                        Thread.sleep(5 * 1000); //设置暂停的时间 5 秒
+                        Thread.sleep(15 * 1000); //设置暂停的时间 15 秒
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    Log.d("Demo", "第"+ num +"次结束拍照" );
+                    isTaking = false;
+
                 }
+
+                try {
+                    Thread.sleep(5 * 1000); //设置暂停的时间 5 秒
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
                 CameraActivity.this.finish();
             }
         }).start();
@@ -96,6 +109,8 @@ public class CameraActivity extends Activity {
     //初始化摄像头
     private void initCamera() {
 
+        isTaking = true;
+
         //如果存在摄像头
         if(checkCameraHardware(getApplicationContext()))
         {
@@ -116,6 +131,8 @@ public class CameraActivity extends Activity {
     //对焦并拍照
     private void autoFocus() {
 
+        isTaking = true;
+
         try {
             //因为开启摄像头需要时间，这里让线程睡两秒
             Thread.sleep(2000);
@@ -126,12 +143,21 @@ public class CameraActivity extends Activity {
         //自动对焦
         myCamera.autoFocus(myAutoFocus);
 
+        try {
+            //因为开启摄像头需要时间，这里让线程睡两秒
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         //对焦后拍照
         myCamera.takePicture(null, null, myPicCallback);
     }
 
     //判断是否存在摄像头
     private boolean checkCameraHardware(Context context) {
+
+        isTaking = true;
 
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
             // 设备存在摄像头
@@ -145,6 +171,8 @@ public class CameraActivity extends Activity {
 
     //得到后置摄像头
     private boolean openFacingFrontCamera() {
+
+        isTaking = true;
 
         //尝试开启前置摄像头
         Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
@@ -206,11 +234,33 @@ public class CameraActivity extends Activity {
             //完成拍照后关闭Activity
             //CameraActivity.this.finish();
 
+            BitmapFactory.Options options = new BitmapFactory.Options();
+//               options.inSampleSize =2;
+            options.inPreferredConfig = Bitmap.Config.RGB_565;//必须设置为565，否则无法检测
+
             //将得到的照片进行270°旋转，使其竖直
-            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length, options);
             Matrix matrix = new Matrix();
             matrix.preRotate(270);
             bitmap = Bitmap.createBitmap(bitmap ,0,0, bitmap .getWidth(), bitmap .getHeight(),matrix,true);
+
+
+
+            //FaceDetecor只能读取RGB 565格式的Bitmap，将上面得到的Bitmap进行一次格式转换
+     //       Bitmap myBitmap = bitmap.copy(Bitmap.Config.RGB_565, true);
+          //  String oristring = bitmapToString(bitmap);
+
+            //假设最多有1张脸
+            int MAXfFaces = 1;
+            int numOfFaces = 0;
+            FaceDetector mFaceDetector = new FaceDetector(bitmap.getWidth(),bitmap.getHeight(),MAXfFaces);
+            FaceDetector.Face[] mFace = new FaceDetector.Face[MAXfFaces];
+            //获取实际上有多少张脸
+            numOfFaces = mFaceDetector.findFaces(bitmap, mFace);
+            Log.v("------------->",  "pic num:" + num + "  face num:"+numOfFaces );
+            if(numOfFaces == 1){
+               Log.d("pic num:" + num,  "  eyesDistance:"+ mFace[0].eyesDistance() +"  confidence:"+ mFace[0].confidence());
+            }
 
             //创建并保存图片文件
             File pictureFile = new File(getDir(), "camera"+num+".jpg");
@@ -228,6 +278,9 @@ public class CameraActivity extends Activity {
                 //myCamera = null;
             }
 
+            bitmap.recycle();
+            bitmap = null;
+
             Log.d("Demo", "获取照片成功");
             Toast.makeText(CameraActivity.this, "获取照片成功", Toast.LENGTH_SHORT).show();;
           //  myCamera.stopPreview();
@@ -236,6 +289,17 @@ public class CameraActivity extends Activity {
             //myCamera = null;
         }
     };
+
+    public String bitmapToString(Bitmap bitmap){
+        //将Bitmap转换成字符串
+        String string=null;
+        ByteArrayOutputStream bStream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,bStream);
+        byte[]bytes=bStream.toByteArray();
+        string= Base64.encodeToString(bytes,Base64.DEFAULT);
+        return string;
+    }
+
 
 /*    Handler mHandler = new Handler() {
         public void handleMessage(Message msg) {
